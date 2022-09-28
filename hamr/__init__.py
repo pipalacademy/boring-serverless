d9f0dc9c1e58d691e2b226869aa89837ddea237a
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-09-28 19:06:30 +0530

    Add index page to view apps, and inital UI setup

--- hamr/__init__.py
+++ hamr/__init__.py
@@ -3,21 +3,52 @@ from pathlib import Path
 
 
 base_path = Path(__file__).parent.parent
-apps_dir = base_path / "apps"
+APPS_DIR = base_path / "apps"
 
 
 class UserApp:
-    def __init__(self, git_url, git_dir):
+    def __init__(self, name, git_url, git_dir):
+        self.name = name
+        self.app_root = APPS_DIR / name
         self.git_url = git_url
         self.git_dir = git_dir
 
+    def init_dirs(self):
+        """Creates necessary directories
+
+        Note: this doesn't create a `site` directory because it
+        is a special directory.
+        If it exists, then the web server must only serve files
+        from that directory and not run any dynamic scripts.
+        """
+        (self.app_root / "bin").mkdir(exists_ok=True)
+        (self.app_root / "logs").mkdir(exists_ok=True)
+        (self.app_root / "public").mkdir(exists_ok=True)
+        (self.app_root / "private").mkdir(exists_ok=True)
+        (self.app_root / "tmp").mkdir(exists_ok=True)
+
+    def git_clone(self):
+        git.Repo.clone_from(self.git_url, self.git_dir)
+
+    def git_pull(self):
+        git.Repo(self.git_dir).remotes.origin.pull()
+
     def deploy(self):
         if (self.git_dir / ".git").exists():
-            repo = git.Repo(self.git_dir)
-            repo.remotes.origin.pull()
+            self.git_pull()
         else:
-            # repo = git.Repo.clone_from(self.git_url, self.git_dir)
+            # self.git_clone()
             raise Exception("App dir doesn't exist")
+
+
+def get_apps():
+    apps = []
+
+    # get sub directories
+    for appdir in APPS_DIR.glob("*/"):
+        apps.append(get_app_from_dir(appdir))
+
+    return apps
 
 
 def get_gh_app(repo_owner, repo_name):
@@ -31,14 +62,22 @@ def get_repo_url(owner_name, repo_name):
 
 
 def get_app_by_name(app_name):
-    app_dir = apps_dir / app_name
+    app_dir = APPS_DIR / app_name
     if app_dir.is_dir():
-        return UserApp(None, app_dir)
+        return get_app_from_dir(app_dir)
 
     return None
 
 
-def get_repo_dir(owner_name, repo_name):
-    apps_dir.mkdir(exist_ok=True)
+def get_app_from_dir(app_dir):
+    repo = git.Repo(app_dir)
+    git_url = (None if repo.bare or not repo.remotes
+               else next(repo.remote("origin").urls))
 
-    return apps_dir / owner_name
+    return UserApp(name=app_dir.name, git_url=git_url, git_dir=app_dir)
+
+
+def get_repo_dir(owner_name, repo_name):
+    APPS_DIR.mkdir(exist_ok=True)
+
+    return APPS_DIR / owner_name
--- hamr/api.py
+++ hamr/api.py
@@ -1,9 +1,9 @@
 import yaml
 from git.exc import GitError
 
-from flask import Flask
+from flask import Flask, render_template
 
-from . import base_path, get_app_by_name
+from . import base_path, get_app_by_name, get_apps
 
 
 app = Flask(__name__)
@@ -16,13 +16,20 @@ def get_config():
     return config
 
 
-@app.route("/")
+@app.route("/api/")
 def health_check():
     return "Works!\n"
 
 
+@app.route("/")
+def index():
+    apps = get_apps()
+    return render_template("index.html", apps=apps)
+
+
 @app.route("/apps/<app_name>/deploy", methods=["POST"])
 def deploy(app_name):
+    # should the route for this endpoint be changed to begin with /api/ ?
     app = get_app_by_name(app_name)
     if app is None:
         return {
