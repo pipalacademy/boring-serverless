b57925f518ce902db2ba87cce9d054e77e504b9f
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-10-19 13:06:37 +0530

    logs: continue gracefully when log directory doesn't exist

--- split_logs.py
+++ split_logs.py
@@ -2,9 +2,13 @@ import os
 import sys
 
 
-def get_log_file(hamr_dir, host, log_type):
+def get_log_file(logdir, logtype):
     subdomain = host.split(".", maxsplit=1)[0]
-    return os.path.join(hamr_dir, f"apps/{subdomain}/logs/{log_type}.log")
+    return os.path.join(logdir, f"{logtype}.log")
+
+def get_log_dir(hamr_dir, host):
+    subdomain = host.split(".", maxsplit=1)[0]
+    return os.path.join(hamr_dir, "apps", subdomain, "logs")
 
 if len(sys.argv) < 3:
     raise Exception("log_type and hamr_dir args are required")
@@ -13,5 +17,11 @@ hamr_dir, log_type= sys.argv[1:3]
 
 for line in sys.stdin:
     host, rest = line.split(maxsplit=1)
-    with open(get_log_file(hamr_dir, host, log_type), "a") as f:
+    log_dir = get_log_dir(hamr_dir, host)
+    if not os.path.exists(log_dir):
+        # graceful exit, maybe app doesn't exist
+        print(f"logdir ('{log_dir}') does not exist.", file=sys.stderr)
+        continue
+
+    with open(get_log_file(log_dir, log_type), "a") as f:
         f.write(rest)
